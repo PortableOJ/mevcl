4b9b82b0714a7be96cd6446b1b1deff0ef2d51d7
shiroha <keqing.hu@icloud.com>
2021-12-13 00:46:30 +0800

    fix: 修复 markdown 数学公式块错误地解析为内联

--- src/components/v1/markdown/main.js
+++ src/components/v1/markdown/main.js
@@ -1,76 +1,87 @@
 import katex from 'katex'
 import {marked} from "marked";
 
+import Vue from "vue";
+import MarkdownBlockCode from "./MarkdownBlockCode";
+
+// eslint-disable-next-line no-unused-vars
+const markdownBlockCodeConstructor = Vue.extend(MarkdownBlockCode)
+
 const Markdown = function () {
 
     // noinspection JSUnusedGlobalSymbols
-    const inlineKatex = {
-        name: 'mathjaxInline',
+    const katexLaxer = {
+        name: 'mathjax',
         level: 'inline',
         start(src) {
             return src.match(/\$.*\$/)?.index;
         },
         tokenizer(src) {
-            const rule = /^([^$]*)\$([^$]*)\$(.*)$/;
-            const match = rule.exec(src);
-            if (match) {
+            const blockRule = /^([^$]*)\$\$([^$]*)\$\$(.*)$/
+            const blockMatch = blockRule.exec(src)
+            if (blockMatch) {
                 return {
-                    type: 'mathjaxInline',
-                    raw: match[0],
-                    left: this.lexer.inlineTokens(match[1].trim()),
-                    text: match[2].trim(),
-                    right: this.lexer.inlineTokens(match[3].trim()),
-                };
+                    type: 'mathjax',
+                    level: 'block',
+                    raw: blockMatch[0],
+                    left: this.lexer.inlineTokens(blockMatch[1].trim()),
+                    text: blockMatch[2].trim(),
+                    right: this.lexer.inlineTokens(blockMatch[3].trim()),
+                }
+            }
+
+            const inlineRule = /^([^$]*)\$([^$]*)\$(.*)$/
+            const inlineMatch = inlineRule.exec(src)
+            if (inlineMatch) {
+                return {
+                    type: 'mathjax',
+                    level: 'inline',
+                    raw: inlineMatch[0],
+                    left: this.lexer.inlineTokens(inlineMatch[1].trim()),
+                    text: inlineMatch[2].trim(),
+                    right: this.lexer.inlineTokens(inlineMatch[3].trim()),
+                }
             }
         },
         renderer(token) {
             const left = this.parser.parseInline(token.left)
             const right = this.parser.parseInline(token.right)
-            if (token.text.length === 0) return `${left}${right}`
             // noinspection JSUnresolvedFunction
             const html = katex.renderToString(token.text, {throwOnError: false})
-            return `${left}${html}${right}`
+            const math = token.level === 'block' ? `<div>${html}</div>` : html
+            return `${left}${math}${right}`
         }
     }
 
-    // noinspection JSUnusedGlobalSymbols
-    const blockKatex = {
-        name: 'mathjaxBlock',
-        level: 'block',
-        start(src) {
-            return src.match(/\$\$.*\$\$/)?.index;
-        },
-        tokenizer(src) {
-            const rule = /^([^$]*)\$\$([^$]*)\$\$(.*)$/;
-            const match = rule.exec(src);
-            if (match) {
-                return {
-                    type: 'mathjaxBlock',
-                    raw: match[0],
-                    left: this.lexer.inlineTokens(match[1].trim()),
-                    text: match[2].trim(),
-                    right: this.lexer.inlineTokens(match[3].trim()),
-                };
-            }
-        },
-        renderer(token) {
-            const left = this.parser.parseInline(token.left)
-            const right = this.parser.parseInline(token.right)
-            if (token.text.length === 0) return `${left}<br>${right}`
-            // noinspection JSUnresolvedFunction
-            const html = katex.renderToString(token.text, {throwOnError: false})
-            return `${left}<br>${html}<br>${right}`
-        }
-    }
+    marked.use({extensions: [katexLaxer]});
 
-    marked.use({extensions: [blockKatex, inlineKatex]});
+    const renderStyle = new marked.Renderer();
+
+    let mountId = 0
+    let mountDist = {}
+    renderStyle.code = function (code, language) {
+        mountId++
+        mountDist[mountId] = new markdownBlockCodeConstructor({
+            data: {code: code, language: language}
+        })
+        return `<div id="MOUNT-${mountId}"></div>`
+    }
 
     const markdownParser = function (text) {
-        return marked(text)
+        console.log(marked.lexer(text))
+        return marked(text, {renderer: renderStyle})
+    }
+
+    const markdownMount = function () {
+        for (const mountDistKey in mountDist) {
+            mountDist[mountDistKey].$mount(document.getElementById(`MOUNT-${mountDistKey}`))
+        }
+        mountDist = {}
     }
 
     return function (Vue) {
         Vue.prototype.$markdown = markdownParser
+        Vue.prototype.$renderer = markdownMount
     }
 }
 
