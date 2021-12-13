ec6b82aa4ca470b422fbdf0500f3374fc9eb90cc
shiroha <keqing.hu@icloud.com>
2021-12-13 12:59:25 +0800

    fix: 修复了过长字符串导致 InputTextarea.vue 的显示错误

--- src/components/v1/InputTextarea.vue
+++ src/components/v1/InputTextarea.vue
@@ -57,6 +57,7 @@ export default {
     -webkit-user-modify: read-write-plaintext-only;
     -moz-user-modify: read-write-plaintext-only;
     user-modify: read-write-plaintext-only;
+    overflow: scroll;
 }
 
 .input-label {
