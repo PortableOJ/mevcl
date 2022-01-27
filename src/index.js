371a3ef0126e68c2fc0698151dad13733470cfda
shiroha <keqing.hu@icloud.com>
2022-01-27 22:04:40 +0800

    feat: 增加了导入使用内容

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import InputImage from "./components/v1/InputImage";
 import InputSelect from "./components/v1/InputSelect";
 import InputSlider from "./components/v1/InputSlider";
 import InputText from "./components/v1/InputText";
+import InputTextarea from "./components/v1/InputTextarea";
 import Link from "./components/v1/Link";
+import MarkdownEdit from "./components/v1/MarkdownEdit";
 import NavMenu from "./components/v1/NavMenu";
 import Pagination from "./components/v1/Pagination";
 import Steps from "./components/v1/Steps";
@@ -19,6 +21,7 @@ import Tag from "./components/v1/Tag";
 
 import MessageBox from "./components/v1/messageBox/main";
 import Toast from "./components/v1/toast/main";
+import Markdown from './components/v1/markdown/main'
 
 const components = [
     MiniLoading,
@@ -32,7 +35,9 @@ const components = [
     InputSelect,
     InputSlider,
     InputText,
+    InputTextarea,
     Link,
+    MarkdownEdit,
     NavMenu,
     Pagination,
     Steps,
@@ -47,10 +52,11 @@ const install = function (Vue) {
 
     Vue.use(Toast)
     Vue.use(MessageBox)
+    Vue.use(Markdown)
 }
 
 export default {
-    version: '0.1.0',
+    version: '0.3.0',
     install,
 
     MiniLoading,
@@ -64,7 +70,9 @@ export default {
     InputSelect,
     InputSlider,
     InputText,
+    InputTextarea,
     Link,
+    MarkdownEdit,
     NavMenu,
     Pagination,
     Steps,
